8aa56d522d13ffd1fb58045065cfe4e3418b7a31
blackout <1157185904@qq.com>
2021-05-31 20:14:08 +0800

    2021-05-31

--- src/components/worksSkeleton/worksSkeleton.vue
+++ src/components/worksSkeleton/worksSkeleton.vue
@@ -45,86 +45,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-<<<<<<< HEAD
-	.container{
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-evenly;
-	}
-	.work-warter {
-		width: 48vw;
-		border-radius: 15rpx 15rpx;
-		margin: 10rpx 5rpx;
-		background-color: #ffffff;
-		overflow: hidden;
-		position: relative;
-	}
-	.work-image {
-		height: 400rpx;
-		width: 100%;
-		border-radius: 15rpx 15rpx 0 0;
-	}
-	
-	.work-title {
-		margin: 25rpx 25rpx 20rpx 25rpx;
-		height: 40rpx;
-		font-size: 32rpx;
-		color: $u-main-color;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		display: -webkit-box;
-		word-break: break-all;
-		-webkit-box-orient: vertical;
-		-webkit-line-clamp: 2;
-	}
-	.work-avatar {
-		margin-right: 10rpx;
-		width: 40rpx;
-		height: 40rpx;
-		border-radius: 50%;
-		float: left;
-	}
-	.work-author {
-		width: 20vw;
-		height: 45rpx;
-		font-size: 25rpx;
-		font-weight: lighter;
-		float: left;
-	}
-	.work-author-box {
-		display: inline-block;
-		padding: 0 25rpx;
-		margin-bottom: 20rpx;
-	}
-	
-	.loading .work-image,
-	.loading .work-title,
-	.loading .work-author-box .work-author,
-	.loading .work-author-box .work-avatar {
-		background-color: $uni-text-color-grey;
-		background: linear-gradient(100deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.5) 50%, rgba(255, 255, 255, 0) 60%) $uni-text-color-grey;
-		background-size: 200% 100%;
-		background-position-x: 180%;
-		animation: 1s loading ease-in-out infinite;
-	}
-	
-	@keyframes loading {
-		to {
-			background-position-x: -20%;
-            background-color: orange;
-		}
-	}
-	
-	.loading .work-title {
-		border-radius: 4px;
-		animation-delay: 0.05s;
-	}
-	
-	.loading .work-author {
-		border-radius: 4px;
-		animation-delay: 0.06s;
-	}
-=======
 .container {
   display: flex;
   flex-wrap: wrap;
@@ -207,5 +127,4 @@ export default {
   border-radius: 4px;
   animation-delay: 0.06s;
 }
->>>>>>> 1ac7f9078b98b96833fc51e51121dbdfc4013a0b
 </style>
